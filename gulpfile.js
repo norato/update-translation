d6cac23762a9428e2f8a0df1d0ba962d77eb48f1
Felipe Norato Lacerda <fnoratol@gmail.com>
2016-01-13 00:49:40 +0000

    trigger replace strategy

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,17 @@ var gulp = require("gulp");
 var jeditor = require("gulp-json-editor");
 
 var languages = ['de-DE', 'en-GR', 'fr-FR', 'nl-NL'];
+var keyword   = 'KEY_TO_ADD';
 
 gulp.task('default',function () {
 	languages.forEach(function (language) {
 		var file = ["translations/translation-", language,".json"].join("");
+        
+        var retVar = {}
+        retVar[keyword] = language;
+          
 		gulp.src(file)
-		  .pipe(jeditor({
-		    'version': '1.2.3'
-		  }))
+		  .pipe(jeditor(retVar))
 		  .pipe(gulp.dest("changed"));
 	});
 })
