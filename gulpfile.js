88f6b6befc36f62b3b3d20f60ad0a9717d5cc08c
Felipe Norato <fnoratol@gmail.com>
2016-01-13 09:16:51 +0000

    Define strategy to translate

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,26 @@
 var gulp = require("gulp");
 var jeditor = require("gulp-json-editor");
 
-var languages = ['de-DE', 'en-GR', 'fr-FR', 'nl-NL'];
+
+var languages = [
+	{culture : 'de-DE', translation: ''}, 
+	{culture : 'en-GR', translation: ''}, 
+	{culture : 'fr-FR', translation: ''}, 
+	{culture : 'nl-NL', translation: ''}
+];
 var keyword   = 'KEY_TO_ADD';
 
-gulp.task('default',function () {
+gulp.task('default',translate());
+
+function translate() {
 	languages.forEach(function (language) {
-		var file = ["translations/translation-", language,".json"].join("");
+		var file = ["translations/translation-", language.culture,".json"].join("");
         
         var retVar = {}
-        retVar[keyword] = language;
+        retVar[keyword] = language.translation;
           
 		gulp.src(file)
 		  .pipe(jeditor(retVar))
 		  .pipe(gulp.dest("changed"));
 	});
-})
+}
