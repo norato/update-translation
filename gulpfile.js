65664a7285bd94ff318bbaf48f4ad107891d9734
Felipe Norato <fnoratol@gmail.com>
2016-01-13 10:25:21 +0000

    Add gulp-prompt to get user keypress values and replace on json files, and add a simple report

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,48 @@
 var gulp = require("gulp");
 var jeditor = require("gulp-json-editor");
-
+var prompt = require("gulp-prompt");
 
 var languages = [
-	{culture : 'de-DE', translation: ''}, 
-	{culture : 'en-GR', translation: ''}, 
-	{culture : 'fr-FR', translation: ''}, 
-	{culture : 'nl-NL', translation: ''}
+    {culture : 'de-DE', translation: ''},
+    {culture : 'en-GR', translation: ''},
+    {culture : 'fr-FR', translation: ''},
+    {culture : 'nl-NL', translation: ''}
 ];
-var keyword   = 'KEY_TO_ADD';
+var keyword   = '';
 
-gulp.task('default',translate());
+gulp.task('default',function(){getValues()});
+
+function getValues() {
+    gulp.src(["translations/"])
+        .pipe(prompt.prompt([{
+            type: 'input',
+            name: 'first',
+            message: 'First question?'
+        },
+        ], function(res){
+            keyword = res.first;
+            translate();
+        })
+        .once('end', function () {
+            report();
+            process.exit();
+        })
+    );
+}
 
 function translate() {
-	languages.forEach(function (language) {
-		var file = ["translations/translation-", language.culture,".json"].join("");
-        
+    languages.forEach(function (language) {
+        var file = ["translations/translation-", language.culture,".json"].join("");
+
         var retVar = {}
         retVar[keyword] = language.translation;
-          
-		gulp.src(file)
-		  .pipe(jeditor(retVar))
-		  .pipe(gulp.dest("changed"));
-	});
+
+        gulp.src(file)
+          .pipe(jeditor(retVar))
+          .pipe(gulp.dest("changed"));
+    });
+}
+
+function report () {
+    console.log("Add key: ", keyword);
 }
